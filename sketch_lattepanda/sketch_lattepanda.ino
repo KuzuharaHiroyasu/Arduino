6ded0dbeb5ce9bacda84fd21ce7d4bc3aa9de84c
KuzuharaHiroyasu <43770961+KuzuharaHiroyasu@users.noreply.github.com>
2018-12-26 17:35:59 +0900

    バイブ対応 （インデント自動補正）

--- sketch_lattepanda/sketch_lattepanda.ino
+++ sketch_lattepanda/sketch_lattepanda.ino
@@ -12,6 +12,7 @@ DS3231 clock;
 RTCDateTime dt;
 const int LED = 13;
 const int POWER = 22;
+const int VIB = 6;
 int incomingByte = 0;   // for incoming serial data
 int interval = 250;
 int i, k = 0;
@@ -22,8 +23,10 @@ void setup() {
   Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
   pinMode(LED, OUTPUT);
   pinMode(POWER, OUTPUT);
+  pinMode(VIB, OUTPUT);
   digitalWrite(LED, LOW);
   digitalWrite(POWER, HIGH);
+  digitalWrite(VIB, LOW);
   clock.begin();
 }
 
@@ -32,86 +35,101 @@ void loop() {
 
   switch (pattern)
   {
-  case off_pattern: //消灯
-    break;
-  case ready_pattern: //点滅
-    digitalWrite(LED, HIGH);
-    delay(interval);
-    digitalWrite(LED, LOW);
-    delay(interval);
-    i++;
-    if(i == 20 && !power_on) //10秒後に電源ON
-    {
-      digitalWrite(POWER, LOW);
-      delay(2000);
-      digitalWrite(POWER, HIGH);
-      power_on = true;
-      interval = 1000;
-    }
-    break;
-  case error_pattern: //点灯
-    break;
-  case end_pattern: // ウィンドウズ終了
-    for(int ii = 0; ii < 3; ii++)
-    {
+    case off_pattern: //消灯
+      break;
+    case ready_pattern: //点滅
       digitalWrite(LED, HIGH);
       delay(interval);
       digitalWrite(LED, LOW);
       delay(interval);
-    }
-    delay(1000);
-    break;
-  default:
-    break;
+      i++;
+      if (i == 20 && !power_on) //10秒後に電源ON
+      {
+        digitalWrite(POWER, LOW);
+        delay(2000);
+        digitalWrite(POWER, HIGH);
+        power_on = true;
+        interval = 1000;
+      }
+      break;
+    case error_pattern: //点灯
+      break;
+    case end_pattern: // ウィンドウズ終了
+      for (int ii = 0; ii < 3; ii++)
+      {
+        digitalWrite(LED, HIGH);
+        delay(interval);
+        digitalWrite(LED, LOW);
+        delay(interval);
+      }
+      delay(1000);
+      break;
+    default:
+      break;
   }
 
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
     switch (incomingByte)
     {
-    case 0: //準備完了
-      digitalWrite(LED, LOW);
-      pattern = off_pattern;
-      break;
-    case 1: //エラー
-      digitalWrite(LED, HIGH);
-      pattern = error_pattern;
-      break;
-    case 2: // 時刻
-      dt = clock.getDateTime();
-      Serial.print("Y");
-      delay(50);
-      Serial.print(dt.year);
-      delay(50);
+      case 0: //準備完了
+        digitalWrite(LED, LOW);
+        pattern = off_pattern;
+        break;
+      case 1: //エラー
+        digitalWrite(LED, HIGH);
+        pattern = error_pattern;
+        break;
+      case 2: //時刻
+        dt = clock.getDateTime();
+        Serial.print("Y");
+        delay(50);
+        Serial.print(dt.year);
+        delay(50);
 
-      Serial.print("M");
-      delay(50);
-      Serial.print(dt.month);
-      delay(50);
+        Serial.print("M");
+        delay(50);
+        Serial.print(dt.month);
+        delay(50);
 
-      Serial.print("D");
-      delay(50);
-      Serial.print(dt.day);
-      delay(50);
-      
-      Serial.print("h");
-      delay(50);
-      Serial.print(dt.hour);
-      delay(50);
+        Serial.print("D");
+        delay(50);
+        Serial.print(dt.day);
+        delay(50);
 
-      Serial.print("m");
-      delay(50);
-      Serial.print(dt.minute);
-      delay(50);
+        Serial.print("h");
+        delay(50);
+        Serial.print(dt.hour);
+        delay(50);
 
-      Serial.print("s");
-      delay(50);
-      Serial.print(dt.second);
-      delay(50);
+        Serial.print("m");
+        delay(50);
+        Serial.print(dt.minute);
+        delay(50);
 
-      Serial.print("END");
-    default:
-      break;
+        Serial.print("s");
+        delay(50);
+        Serial.print(dt.second);
+        delay(50);
+
+        Serial.print("END");
+        break;
+      case 3: //バイブ
+        digitalWrite(VIB, HIGH);
+        delay(100);
+        digitalWrite(VIB, LOW);
+        delay(100);
+        digitalWrite(VIB, HIGH);
+        delay(100);
+        digitalWrite(VIB, LOW);
+        delay(100);
+        digitalWrite(VIB, HIGH);
+        delay(100);
+        digitalWrite(VIB, LOW);
+
+        break;
+      default:
+        break;
     }
   }
   delay(10);
